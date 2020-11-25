81dc491fde04a4be7bc0638de4131897eaef8cf4
Veritas-03 <anupamyerramsetti@gmail.com>
2020-11-26 03:15:44 +0530

    index.php dark theme

--- index.php
+++ index.php
@@ -25,9 +25,58 @@ session_start();
         <link rel="stylesheet" href="assets/CSS/main.css">
 </head>
 <body>
-	<div class="base mt-1 d-flex row justify-content-between">
+	<div class="base profile-bg" style="font-family: OpenSans-Regular;">
+		<nav class="navbar navbar-expand-lg navbar-dark nav">
+        <a class="navbar-brand " href="index.html">LinkedStacks</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          
+          <form class="form-inline my-2 my-lg-0 mr-auto ml-auto">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search Project">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+
+          <ul class="navbar-nav ml-auto align-self-center">
+            
+            <li class="nav-item  ">
+              <a class="nav-link" href="index.html">Home
+                
+          
+            <li class="nav-item ">
+              <a class="nav-link" href="index.html">
+                  <span><?php echo $_SESSION['username']; ?></span>
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item ">
+              <a class="nav-link" href="index.html">
+                  Name
+              </a>
+            </li>
+            
+            <li class="nav-item active ">
+                <a class="nav-link" href="profile.html">My Profile
+                  
+                </a>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link" href="editBio.html">Edit Profile
+                  
+                </a>
+              <li class="nav-item  ">
+                <a class="nav-link" href="#">Logout
+                  
+                </a>
+              </li>
+        
+           
+          </ul>
+        </div>
+      </nav>
+      <div class="d-flex row justify-content-between">
 		<div class="newPostSection col-md-7 mt-4">
-			<p class="ml-md-5">What's New-</p>
+			<p class="ml-5">What's New-</p>
 			<div class="postsColumn">
 				<div class="p-3 post">
 					<p>post title</p>
@@ -42,15 +91,21 @@ session_start();
 					<p>post tags</p>
 				</div>
 			</div>
-			<p id="more">more</p>
+			<p class="ml-5" id="more">more</p>
 		</div>
-		<div class="otherSection d-flex flex-column justify-content-star col-md-5 col-lg-4 mt-4">
+		<div class="otherSection ml-sm-5 d-flex flex-column justify-content-star col-md-5 col-lg-4 mt-4">
 			<div class="p-3 leaderBoard">
 				<p>Top Contributors</p>
 				<div class="d-flex flex-column justify-content-around mt-2">
-					<p>lead</p>
-					<p>er</p>
-					<p>board</p>
+					<div class="mb-2 leaders">
+						<p>lead</p>
+					</div>
+					<div class="mb-2 leaders">
+						<p>er</p>
+					</div>
+					<div class="mb-2 leaders">
+						<p>board</p>
+					</div>
 				</div>
 			</div>
 			<div class="p-3 mt-3 newPost">
@@ -65,9 +120,10 @@ session_start();
 				</div>
 			</div>			
 		</div>
+	   </div>
 	</div>
 	<div class="footer px-5 py-4">
-		<hr style="background-color: red;">
+		<hr style="background-color: #375a7f;">
 		<div class="mt-4 d-flex flex-row justify-content-between">
 			<div class="socialMedia">
 				<a href="https://www.linkedin.com">
