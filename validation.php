983af0f50b003cf2ec52a10408ccf70d1431f1a7
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2020-12-03 01:36:02 +0530

    session variable set

--- validation.php
+++ validation.php
@@ -1,22 +1,10 @@
 <?php
 
-session_start();
-
-$con = mysqli_connect('127.0.0.1:3307', 'root' , '');
-if($con)
-{
-    echo "Connection Sucessful";
-}
-else 
-{
-    echo "Connection Failed";
-}
+require_once("config.php");
 
 
-mysqli_select_db($con ,'linkedstacks');
-$username = $_POST['username'];
-$pass = $_POST['pass'];
-
+$username=$_POST['username'];
+$pass=$_POST['pass'];
 $q = "SELECT * FROM credentials WHERE username='$username' && password ='$pass' ";
 
 $result = mysqli_query($con, $q);
@@ -24,7 +12,9 @@ $num = mysqli_num_rows($result);
 
 if($num==1)
 {
-    $_SESSION['username'] = $username;
+     $loggedUser= mysqli_fetch_array($result);
+    $_SESSION['User ID'] = $loggedUser['User ID'];
+
     header('location:index.php');
 }
 else
