d013983f31366f09edd4ad29f0e956a686b1fc35
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2020-12-03 13:08:18 +0530

    color patch

--- editbio.php
+++ editbio.php
@@ -15,7 +15,7 @@
 </header>
 <body class="profile-bg">
     <nav class="navbar navbar-expand-lg navbar-dark nav">
-        <a class="navbar-brand " href="index.html">LinkedStacks</a>
+        <a class="navbar-brand " href="index.php">LinkedStacks</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -30,15 +30,15 @@
           <ul class="navbar-nav ml-auto align-self-center">
             
             <li class="nav-item  ">
-              <a class="nav-link" href="index.html">Home</a>
+              <a class="nav-link" href="index.php">Home</a>
             
             <li class="nav-item  ">
-                <a class="nav-link" href="profile.html">My Profile
+                <a class="nav-link" href="profile.php">My Profile
                   
                 </a>
               </li>
               <li class="nav-item active">
-                <a class="nav-link" href="editBio.html">Edit Profile
+                <a class="nav-link" href="editBio.php">Edit Profile
                   
                 </a>
               <li class="nav-item  ">
